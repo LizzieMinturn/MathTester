664b1055a2c95aaa695aaa215fd116a19761e294
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-01-29 07:34:56 -0500

    declared and initialized firstWord (dog) and secondWord (house)

---
+++ src/MathTester.java
@@ -0,0 +1,14 @@
+public class MathTester {
+
+    public static void main(String[]){
+        String firstWord;
+        String secondWord;
+
+        firstWord = "dog"
+        secondWord = "house"
+
+
+
+
+    }
+}
