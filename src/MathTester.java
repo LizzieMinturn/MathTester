e710657ace3f5536d12a2ff0536c98f50ee54de4
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-01-29 07:42:58 -0500

    declared and initialized firstNumber (5) and secondNumber (10)

--- src/MathTester.java
+++ src/MathTester.java
@@ -3,11 +3,15 @@ public class MathTester {
     public static void main(String[] args){
         String firstWord;
         String secondWord;
+        int firstNumber,secondNumber;
 
 
         firstWord = "dog";
         secondWord = "house";
-        
+        firstNumber = 5;
+        secondNumber = 10;
+
+
 
         System.out.println(firstWord+secondWord);
 
