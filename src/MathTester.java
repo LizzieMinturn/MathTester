42c4759fb4ae111b0e88af4451d52a7a661959dd
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-01-29 08:01:32 -0500

    put a block comment on all the equations

--- src/MathTester.java
+++ src/MathTester.java
@@ -11,13 +11,14 @@ public class MathTester {
         firstNumber = 5;
         secondNumber = 10;
 
-        System.out.println(firstWord+secondWord);
+        /*System.out.println(firstWord+secondWord);
         System.out.println(firstWord+(firstNumber+secondNumber));
         System.out.println("firstNumber + secondNumber is " + (firstNumber+secondNumber));
         System.out.println("firstNumber - secondNumber is " + (firstNumber-secondNumber));
         System.out.println("secondNumber / firstNumber is " + (secondNumber/firstNumber));
         System.out.println("firstNumber * secondNumber is " + (firstNumber*secondNumber));
-        System.out.println("the modulous of the secondNumber by the firstNumber is " + (secondNumber+firstNumber));
+        System.out.println("the modulous of the secondNumber by the firstNumber" +
+                " is " + (secondNumber%firstNumber));*/
 
 
         }
